a0da94be0ae335e835a15a5ffb4e1dae2994c5e6
Nikolaj Hey Hinnerskov <nhey@900901.xyz>
2026-03-06 17:49:15 +0100

    Pin nixpkgs to indexfn version.

--- docker.nix
+++ docker.nix
@@ -1,21 +1,13 @@
 { pkgs ? import (fetchTarball
-  "https://github.com/NixOS/nixpkgs/archive/794e497348ea2019c258aeefc2c9526a0873f9be.tar.gz")
+  "https://github.com/NixOS/nixpkgs/archive/0f1874526206d8a2d6f0a3925618cc45ac83049f.tar.gz")
   { } }:
 let
-  futhark-original0 = import ./artifact/futhark-original/default.nix { };
-  futhark-automap0 = import ./artifact/futhark-automap/default.nix { };
-  futhark-original = futhark-original0.overrideAttrs (old: {
+  futhark-PropProp0 = import ./artifact/futhark-PropProp/default.nix { };
+  futhark-PropProp = futhark-PropProp0.overrideAttrs (old: {
     installPhase = ''
       mkdir -p $out/bin
       tar xf futhark-nightly.tar.xz
-      cp futhark-nightly/bin/futhark $out/bin/futhark-original
-    '';
-  });
-  futhark-automap = futhark-automap0.overrideAttrs (old: {
-    installPhase = ''
-      mkdir -p $out/bin
-      tar xf futhark-nightly.tar.xz
-      cp futhark-nightly/bin/futhark $out/bin/futhark-automap
+      cp futhark-nightly/bin/futhark $out/bin/futhark-PropProp
     '';
   });
   artifact = pkgs.copyPathToStore ./artifact;
@@ -26,8 +18,7 @@ in pkgs.dockerTools.buildImage {
   copyToRoot = pkgs.buildEnv {
     name = "image-root";
     paths = with pkgs; [
-      futhark-original
-      futhark-automap
+      futhark-PropProp
 
       # Dependencies
       coreutils
